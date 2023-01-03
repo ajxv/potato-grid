64c818a5de7331cb7f7c56a86ece1d5435b254cb
unknown <abin.xv@gmail.com>
2023-01-03 23:54:57 +0530

    added logging | renamed main script | added helper script

---
+++ helper.py
@@ -0,0 +1,32 @@
+import ccxt
+import sys
+
+import keys, config
+
+# exchange object
+exchange = ccxt.binance({
+    'apiKey': keys.API_KEY,
+    'secret': keys.SECRET_KEY
+})
+
+exchange.set_sandbox_mode(True)  # enable sandbox mode
+
+def cancel_orders():
+    # Cancel all orders for the symbol
+    exchange.cancel_all_orders(config.SYMBOL)
+
+    # Cancel all orders across all symbols
+    # exchange.cancel_all_orders()
+
+def view_orders():
+    # Retrieve all active orders for the symbol
+    orders = exchange.fetch_open_orders(config.SYMBOL)
+
+    # Print the orders data
+    for order in orders:
+        order_info = order['info']
+        print(f"{order_info['symbol']} | {order_info['orderId']} | {order_info['side']} | {order_info['status']}")
+
+if __name__ == "__main__":
+    # get function name as cli-arg
+    globals()[sys.argv[1]]()
--- potato.py
+++ potato.py
@@ -1,8 +1,26 @@
 import ccxt
-import keys, config
 import time
 import sys
- 
+import logging
+
+import keys, config
+
+## logging
+# Set the log level to DEBUG
+logging.basicConfig(level=logging.INFO)
+
+# Set the log format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Create a file handler and set the log format
+file_handler = logging.FileHandler('trading.log')
+file_handler.setFormatter(formatter)
+
+# Add the file handler to the logger
+logger = logging.getLogger()
+logger.addHandler(file_handler)
+
+
 # exchange object
 exchange = ccxt.binance({
     'apiKey': keys.API_KEY,
@@ -17,16 +35,19 @@ buy_orders = []
 sell_orders = []
 
 def create_buy_order(symbol, size, price):
-    print("submitting market limit buy order at {}".format(price))
+    logger.info("==> submitting market limit buy order at {}".format(price))
     order = exchange.create_limit_buy_order(symbol, size, price)
     buy_orders.append(order['info'])
 
 def create_sell_order(symbol, size, price):
-    print("submitting market limit sell order at {}".format(price))
+    logger.info("==> submitting market limit sell order at {}".format(price))
     order = exchange.create_limit_sell_order(symbol, size, price)
     sell_orders.append(order['info'])
 
-def potato():
+def main():
+    logger.info('=> Starting grid trading bot')
+
+    global buy_orders, sell_orders
     # place inital buy orders
     for i in range(config.NUM_BUY_GRID_LINES):
         price = ticker['bid'] - (config.GRID_SIZE * (i + 1))
@@ -42,52 +63,54 @@ def potato():
 
         # check if buy order is closed
         for buy_order in buy_orders:
-            print("checking buy order {}".format(buy_order['id']))
+            logger.info("=> checking buy order {}".format(buy_order['orderId']))
             try:
-                order = exchange.fetch_order(buy_order['id'])
+                order = exchange.fetch_order(buy_order['orderId'], config.SYMBOL)
             except Exception as e:
-                print("request failed, retrying")
+                logger.error(e)
+                logger.warning("=> request failed, retrying")
                 continue
                 
             order_info = order['info']
 
             if order_info['status'] == config.CLOSED_ORDER_STATUS:
-                closed_order_ids.append(order_info['id'])
-                print("buy order executed at {}".format(order_info['price']))
+                closed_order_ids.append(order_info['orderId'])
+                logger.info("=> buy order executed at {}".format(order_info['price']))
                 new_sell_price = float(order_info['price']) + config.GRID_SIZE
-                print("creating new limit sell order at {}".format(new_sell_price))
+                logger.info("=> creating new limit sell order at {}".format(new_sell_price))
                 create_sell_order(config.SYMBOL, config.POSITION_SIZE, new_sell_price)
 
             time.sleep(config.CHECK_ORDERS_FREQUENCY)
 
         # check if sell order is closed
         for sell_order in sell_orders:
-            print("checking sell order {}".format(sell_order['id']))
+            logger.info("=> checking sell order {}".format(sell_order['orderId']))
             try:
-                order = exchange.fetch_order(sell_order['id'])
+                order = exchange.fetch_order(sell_order['orderId'], config.SYMBOL)
             except Exception as e:
-                print("request failed, retrying")
+                logger.error(e)
+                logger.warning("=> request failed, retrying")
                 continue
                 
             order_info = order['info']
 
             if order_info['status'] == config.CLOSED_ORDER_STATUS:
-                closed_order_ids.append(order_info['id'])
-                print("sell order executed at {}".format(order_info['price']))
+                closed_order_ids.append(order_info['orderId'])
+                logger.info("=> sell order executed at {}".format(order_info['price']))
                 new_buy_price = float(order_info['price']) - config.GRID_SIZE
-                print("creating new limit buy order at {}".format(new_buy_price))
+                logger.info("=> creating new limit buy order at {}".format(new_buy_price))
                 create_buy_order(config.SYMBOL, config.POSITION_SIZE, new_buy_price)
 
             time.sleep(config.CHECK_ORDERS_FREQUENCY)
 
         # remove closed orders from list
         for order_id in closed_order_ids:
-            buy_orders = [buy_order for buy_order in buy_orders if buy_order['id'] != order_id]
-            sell_orders = [sell_order for sell_order in sell_orders if sell_order['id'] != order_id]
+            buy_orders = [buy_order for buy_order in buy_orders if buy_order['orderId'] != order_id]
+            sell_orders = [sell_order for sell_order in sell_orders if sell_order['orderId'] != order_id]
 
         # exit if no sell orders are left
         if len(sell_orders) == 0:
             sys.exit("stopping bot, nothing left to sell")
 
 if __name__ == "__main__":
-    potato()
+    main()
